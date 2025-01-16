85bcbee330a681e90bd3422edea7f982980a84cf
CraiteX x REDLITE <rlcraitex@gmail.com>
2025-01-16 21:32:52 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# Simple TODO
+
+**Very simpl ToDo REST API for Python + SQLAlchemy**
+
+## Stack
+- **Python3**
+- **SQLAlchemy (sqlite)**
+- **FastAPI**
+
+## Installation
+
+### 1. Clone the repository
+```bash
+git clone https://github.com/XCraiteX/fast-todo
+```
+
+### 2. Run code on `localhost`
+```bash
+uvicorn todo:app --reload
+```
+
+### 3. Use Postman for tests
+
+## API Functions
+
+### 1. Get all tasks | `GET`
+ 
+`/get_tasks`
+
+**Returns all tasks by classes in JSON**
+
+
+### 2. Create Task | `POST`  
+
+`/create_task`
+
+**Creating task and returns "Successfully"**
+
+- **"title"**: str
+- **"content"**: str
+
+**Example:**
+```json
+{
+    "title": "Test task",
+    "content": "Target"
+}
+```
+
+### 3. Update Task | `PUT`
+
+`/update_task/{task_id}`
+
+- **"title"**: str
+- **"content"**: str
+- **"completed"**: bool
+
+**Example:**
+`/update_task/3
+```json
+{
+    "title": "Task 3",
+    "content": "New content"
+}
+```
+
+### 4. Delete Task | `DELETE`
+
+`/delete_task/{task_id}`
+
+**Delete task and returns "Successfully"**
---
+++ todo.py
@@ -0,0 +1,77 @@
+from fastapi import FastAPI
+from sqlalchemy.orm import DeclarativeBase, Session, mapped_column, Mapped
+from sqlalchemy import create_engine, select
+from sqlalchemy.types import String, Integer, DateTime, Boolean
+from pydantic import BaseModel
+from typing import Optional
+
+
+from datetime import datetime
+
+app = FastAPI()
+
+# Default Data Class
+class Base(DeclarativeBase):
+    pass
+
+# Create Engine
+engine = create_engine(url='sqlite:///base.db')
+
+# Notes Table
+class Tasks(Base):
+    __tablename__ = 'Notes'
+
+    id: Mapped[Integer] = mapped_column(Integer, autoincrement=True, primary_key=True)
+    title: Mapped[String] = mapped_column(String, default='Title..')
+    content: Mapped[String] = mapped_column(String, default='')
+    completed: Mapped[Boolean] = mapped_column(Boolean, default=False)
+    date: Mapped[DateTime] = mapped_column(DateTime, default=datetime.now())
+
+# Note for validation
+class Task(BaseModel):
+    title: str
+    content: str
+    completed: Optional[bool] = False
+
+
+# Creating Tables
+Base.metadata.create_all(engine)
+
+
+@app.get('/get_tasks')
+async def get_tasks():
+    tasks = Session(engine).query(Tasks).all()
+    return {"tasks": tasks}
+
+
+@app.post('/create_task')
+async def create_task(task: Task):
+    with Session(engine) as db:
+        obj = Tasks(title=task.title, content=task.content)
+        db.add(obj)
+        db.commit()
+        return {'details': 'Successfully!'}
+
+
+@app.put('/update_task/{task_id}')
+async def update_task(task_id: str, new: Task):
+    with Session(engine) as db:
+        task = db.query(Tasks).filter(Tasks.id == task_id).first()
+        if task:
+            task.title = new.title
+            task.content = new.content
+            if new.completed:
+                task.completed = new.completed
+            db.commit()
+            return {"details": "Successfully!"}
+
+
+@app.delete('/delete_task/{task_id}')
+async def delete_task(task_id: int): 
+    with Session(engine) as db:
+        task = db.query(Tasks).filter(Tasks.id == task_id).first()
+        db.delete(task)
+        db.commit()
+        return {'details': "Successfully!"}
+
+# uvicorn todo:app --reload
